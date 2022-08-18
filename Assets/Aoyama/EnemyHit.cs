9c47034c5aedae46728acbf2ee167abb5a883846
aoyama_saito <saitomarimo1229@gmail.com>
2022-08-18 14:36:15 +0900

    npcのhit処理を仮追加

---
+++ Assets/Aoyama/EnemyHit.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    [SerializeField]
+    private string _player1 = "Player1";
+    [SerializeField]
+    private string _player2 = "Player2";
+
+    /// <summary>
+    /// モンスターに接触したときに呼び出す関数
+    /// </summary>
+    /// <param name="hitObject">当たってきたオブジェクト自身</param>
+    public void Hit(GameObject hitObject)
+    {
+        //当たってきたプレイヤーの判定はTag行う
+        if (hitObject.tag == _player1)
+        {
+            Debug.Log("player1に当たった");
+        }
+        else if (hitObject.tag == _player2)
+        {
+            Debug.Log("player2に当たった");
+        }
+    }
+}
