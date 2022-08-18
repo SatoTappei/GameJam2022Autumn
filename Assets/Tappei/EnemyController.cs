db3fdde0fab4abe6db0be37f4cae09d79cf2ca4f
SatouTappei <stutpi.signup@gmail.com>
2022-08-18 16:47:44 +0900

    うろうろする敵を作成

--- Assets/Tappei/EnemyController.cs
+++ Assets/Tappei/EnemyController.cs
@@ -5,30 +5,66 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    [Header("移動速度"), SerializeField] float _speed;
     NavMeshAgent _agent;
-    /// <summary>ターゲットのTransform</summary>
-    Transform _target;
+    /// <summary>移動速度</summary>
+    [Header("移動速度"), SerializeField] float _speed;
+    /// <summary>敵がプレイヤー認知する距離</summary>
+    [Header("敵がプレイヤーを認知する距離"), SerializeField] float _distance;
+    /// <summary>スライムのタグ</summary>
+    [Header("スライムのタグ"), SerializeField] string _slimeTag;
+    /// <summary>勇者のタグ</summary>
+    [Header("勇者のタグ"), SerializeField] string _heroTag;
+    /// <summary>プレイヤーを見失った際に別の地点へうろうろするまでの時間</summary>
+    [Header("うろうろする際の止まっている時間"), SerializeField] float _randomWait;
+    /// <summary>スライムとの距離を計算するのに必要なTransform</summary>
+    Transform _slimeTrans;
+    /// <summary>勇者との距離を計算するのに必要なTransform</summary>
+    Transform _heroTrans;
 
     void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
         _agent.speed = _speed;
+        _slimeTrans = GameObject.FindGameObjectWithTag(_slimeTag).transform;
+        _heroTrans = GameObject.FindGameObjectWithTag(_heroTag).transform;
     }
 
     void Start()
     {
-        
+        // スポーン時にランダムな地点へ移動させる
+        MoveRandomPoint();
     }
 
     void Update()
     {
-        if (_target != null)
+        float slimeDist = (transform.position - _slimeTrans.position).magnitude;
+        float heroDist = (transform.position - _heroTrans.position).magnitude;
+
+        // スライム、もしくは勇者との距離が指定された距離以下だったら追跡する
+        if (slimeDist < _distance || heroDist < _distance)
         {
-            _agent.SetDestination(_target.position);
+            _agent.destination = slimeDist < heroDist ? _slimeTrans.position : _heroTrans.position;
+        }
+        else
+        {
+            // 目標地点の目前まで来たら
+            if (!_agent.pathPending && _agent.remainingDistance < 0.1f && !_agent.isStopped)
+            {
+                // その地点で止めて、指定した秒後に次の地点へ移動させる
+                _agent.isStopped = true;
+                Invoke("MoveRandomPoint", _randomWait);
+            }
         }
     }
 
-    /// <summary>ターゲットをセットして追跡開始</summary>
-    public void StartChase(Transform target) => _target = target;
+    /// <summary>ランダムな位置に移動させる</summary>
+    void MoveRandomPoint()
+    {
+        _agent.isStopped = false;
+
+        float rx = Random.Range(-5.0f, 5.0f);
+        float rz = Random.Range(-5.0f, 5.0f);
+
+        _agent.destination = new Vector3(transform.position.x + rx, transform.position.y, transform.position.z + rz);
+    }
 }
